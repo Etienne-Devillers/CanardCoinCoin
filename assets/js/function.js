1ee20ad86a792e68f09dc1768f9c741906f0d2e1
Etienne Devillers <devillers.etienne80@gmail.com>
2022-02-03 15:15:23 +0100

    Création du fichier function

---
+++ assets/js/function.js
@@ -0,0 +1,124 @@
+// Ce document contient toutes les fonctions qui seront appelées par le script principal
+
+//******************************************************************************************************************//
+// ---------------------------Fonctions propres au panier et la gestion des articles -------------------------------//
+//******************************************************************************************************************//
+
+//------------------Fonction De supression d'un article
+
+function RemoveItem(product_ID)
+
+
+
+//--Fin supression d'un article
+
+//------------------Fonction de supression de tout les articles
+
+function removeAllItem()
+
+
+//--Fin supression de tout les articles
+
+//------------------Fonction augmenter la quantité d'un article
+
+
+function addItem(product_ID)
+
+
+//--Fin augmentation de la quantité
+
+//------------------Fonction calculer la somme pour chaque article
+
+
+function sumItems(product_id, quantity_ID)
+
+
+//--Fin Somme d'un article
+
+//------------------Fonction calculer la somme du panier
+
+
+function sumItemsTotal()
+
+//--Fin Somme du panier
+
+//------------------Fonction Mise à jour du localStorage
+
+
+function updateLocalStorage()
+
+
+//--Fin mise à jour du localStorage
+
+//------------------Fonction Appel du localStorage
+
+
+function getLocalStorage()
+
+
+
+//--Fin appel du localStorage
+
+
+
+//******************************************************************************************************************//
+// -------------------------------------------Fonctions propres à la main ------------------------------------------//
+//******************************************************************************************************************//
+
+//------------------Fonction Fetch Initial et stockage dans une variable
+
+
+function fetchAndSaveIt()
+
+
+
+//--Fin Fetch Initial et stockage dans une variable
+
+//------------------Fonction Creation des cards
+
+
+function createCards()
+
+
+
+//--Fin Creation des cards
+
+
+
+
+//------------------Fonction changement d'affichage en fonction de la catégorie 
+
+
+function changeDisplay(userChoice)
+
+
+
+//--Fin changement d'affichage en fonction de la catégorie 
+
+//------------------Fonction ajout au panier
+
+
+function addToBasket(card.btn)
+
+
+
+//--Fin ajout au panier
+
+//------------------Fonction Zoom sur image
+
+
+function modalZoomImg(zoomBtn)
+
+
+
+//--Fin Zoom sur image
+
+//------------------Fonction ouvrir le panier
+
+
+function openBasketModal(basketBtn)
+
+
+
+//--Fin ouvrir le panier
+
