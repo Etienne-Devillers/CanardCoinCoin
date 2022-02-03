1fb6a17268efd414235e99f3c5b91950cdb9192d
Etienne Devillers <devillers.etienne80@gmail.com>
2022-02-03 17:02:11 +0100

    correctif json

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ let productsBasket = localStorage.getItem('visitor') ? JSON.parse(localStorage.g
 
 localStorage.setItem('visitor', JSON.stringify(productsBasket));
 
+
+fetch("assets/js/ducks.json")
+
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (datas) {
+console.log(datas);
+test.src = datas.goods[0].animals[0].picture
+    });
