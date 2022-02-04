3bec0c1b76f295b8348ba0cca464dc0838fbb11a
Adeline Izri <adeline.rudnicki@gmail.com>
2022-02-04 16:54:15 +0100

    Affichage des catégories sur la page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,56 @@ fetch("assets/js/ducks.json")
     })
     .then(function (datas) {
 
-var ducksDataBase = datas
-// return ducksDataBase
-console.log(ducksDataBase);
+    var ducksDataBase = datas
+    // return ducksDataBase
+    console.log(ducksDataBase);
 
-ducksDataBase.animals.forEach(element => {
-    firstRow.innerHTML += createCards(element)
-    
-    
+    ducksDataBase.animals.forEach(element => {
+        firstRow.innerHTML += createCards(element)
+        
+    });
+
+let classics = document.getElementById('classics');
+classics.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.classics.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
+});
+let fiction = document.getElementById('fiction');
+fiction.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.fiction.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
+});
+let animals = document.getElementById('animals');
+animals.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.animals.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
+});
+let celebrities = document.getElementById('celebrities');
+celebrities.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.celebrities.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
+});
+let professions = document.getElementById('professions');
+professions.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.professions.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
+});
+let sport = document.getElementById('sport');
+sport.addEventListener('click', () => {
+    firstRow.innerHTML = '';
+    ducksDataBase.sport.forEach(element => {
+        firstRow.innerHTML += createCards(element);
+    });
 });
 
 
@@ -29,6 +71,4 @@ ducksDataBase.animals.forEach(element => {
 
 
 
-
-
-    });
+});
