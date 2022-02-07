244a67a801c193a6ba3742d12a5c1f0c50491098
Etienne Devillers <devillers.etienne80@gmail.com>
2022-02-07 16:26:19 +0100

    reparation findIndex

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let productsBasket = localStorage.getItem('visitor') ? JSON.parse(localStorage.g
 
 
 
+
 fetch("assets/js/ducks.json")
 
     .then(function (resp) {
@@ -15,10 +16,12 @@ fetch("assets/js/ducks.json")
     // return ducksDataBase
     console.log(ducksDataBase);
 
+
     ducksDataBase.animals.forEach(element => {
         firstRow.innerHTML += createCards(element)
         
     });
+    refreshButton(ducksDataBase.animals);
 
 let classics = document.getElementById('classics');
 classics.addEventListener('click', () => {
@@ -26,63 +29,53 @@ classics.addEventListener('click', () => {
     ducksDataBase.classics.forEach(element => {
         firstRow.innerHTML += createCards(element);
     });
+    refreshButton(ducksDataBase.classics);
 });
+
 let fiction = document.getElementById('fiction');
 fiction.addEventListener('click', () => {
     firstRow.innerHTML = '';
     ducksDataBase.fiction.forEach(element => {
         firstRow.innerHTML += createCards(element);
     });
+    refreshButton(ducksDataBase.fiction);
 });
+
 let animals = document.getElementById('animals');
 animals.addEventListener('click', () => {
     firstRow.innerHTML = '';
     ducksDataBase.animals.forEach(element => {
         firstRow.innerHTML += createCards(element);
     });
+    refreshButton(ducksDataBase.animals);
 });
+
 let celebrities = document.getElementById('celebrities');
 celebrities.addEventListener('click', () => {
     firstRow.innerHTML = '';
     ducksDataBase.celebrities.forEach(element => {
         firstRow.innerHTML += createCards(element);
     });
+    refreshButton(ducksDataBase.celebrities);
 });
+
 let professions = document.getElementById('professions');
 professions.addEventListener('click', () => {
     firstRow.innerHTML = '';
     ducksDataBase.professions.forEach(element => {
         firstRow.innerHTML += createCards(element);
-    });
+    }); 
+    refreshButton(ducksDataBase.professions);
+    
 });
+
 let sport = document.getElementById('sport');
 sport.addEventListener('click', () => {
     firstRow.innerHTML = '';
     ducksDataBase.sport.forEach(element => {
         firstRow.innerHTML += createCards(element);
     });
+    refreshButton(ducksDataBase.sport);
 });
 
-
-        ducksDataBase.animals.forEach((element) => {
-
-            document.querySelector('.decrementBtn[data-productId=' + element.id + ']').addEventListener('click', (event) => {
-                decrementValueItem(event.currentTarget.dataset.productid) ;
-                console.log("test bouton -")
-            })
-
-            document.querySelector('.incrementBtn[data-productId=' + element.id + ']').addEventListener('click', (event) => {
-                incrementValueItem(event.currentTarget.dataset.productid) ;
-                console.log("test bouton +")
-            })
-
-            document.querySelector('button[data-productId=' + element.id + ']').addEventListener('click', (event) => {
-                addToBasket(event.currentTarget.dataset.productid);
-                document.querySelector('.offcanvas-body').innerHTML += addItem(element.id)
-                
-            })
-        })
-
-
-
-    });
+});
